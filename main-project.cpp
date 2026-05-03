0c02baf7b29a0dd61119f49f098a027a3dd5c973
Yoshiro Nakamura <marcellinhonkai@gmail.com>
2026-05-02 22:25:58 -0300

    temperature float change

--- main-project.cpp
+++ main-project.cpp
@@ -13,37 +13,38 @@ int main(array<System::String ^> ^args)
 	computer->IsGpuEnabled = true; // Inicia o monitoramento de GPU
 	computer->Open();
 
+	int top = Console::CursorTop; // Armazena a posição inicial do cursor para evitar que a tela fique piscando
 	while (true) {
-		Console::Clear(); // Limpa a tela do console para atualizar os dados em tempo real
+		Console::SetCursorPosition(0, top); // Move o cursor de volta para a posição inicial a cada atualização para evitar que a tela fique piscando
 
 		for each (IHardware ^ hardware in computer->Hardware) {
 			if (hardware->HardwareType == HardwareType::Cpu || // Condições que verificam se o hardware é do tipo CPU ou GPU, não é possivel usar E pois o hardware pode ser de um tipo ou de outro, mas não ambos ao mesmo tempo
 				hardware->HardwareType == HardwareType::GpuAmd ||
 				hardware->HardwareType == HardwareType::GpuIntel ||
 				hardware->HardwareType == HardwareType::GpuNvidia) {
-				hardware->Update(); // Atualiza os dados do hardware
+					hardware->Update(); // Atualiza os dados do hardware
 
-				switch (hardware->HardwareType) {
-				case HardwareType::Cpu:
-					Console::WriteLine("CPU: " + hardware->Name);
-					break;
-				case HardwareType::GpuAmd:
-				case HardwareType::GpuIntel:
-				case HardwareType::GpuNvidia:
-					Console::WriteLine("GPU: " + hardware->Name);
-					break;
-				default:
-					Console::WriteLine("Error: Hardware type not recognized.");
-					break;
-				}
-
-				for each (ISensor ^ sensor in hardware->Sensors) {
-					if (sensor->SensorType == SensorType::Temperature && // Verifica se o sensor é do tipo temperatura e se tem um valor válido (maior que 0)
-						sensor->Value.HasValue &&
-						sensor->Value.Value > 0) {
-						Console::WriteLine(sensor->Name + ": " + sensor->Value + " C");
+					switch (hardware->HardwareType) {
+					case HardwareType::Cpu:
+						Console::WriteLine("CPU: " + hardware->Name);
+						break;
+					case HardwareType::GpuAmd:
+					case HardwareType::GpuIntel:
+					case HardwareType::GpuNvidia:
+						Console::WriteLine("GPU: " + hardware->Name);
+						break;
+					default:
+						Console::WriteLine("Error: Hardware type not recognized.");
+						break;
 					}
-				}
+
+					for each (ISensor ^ sensor in hardware->Sensors) {
+						if (sensor->SensorType == SensorType::Temperature && sensor->Value.HasValue && sensor->Value.Value > 0) { // Verifica se o sensor é do tipo temperatura, se tem um valor válido e se o valor é maior que 0 para evitar mostrar temperaturas negativas ou sensores sem leitura.
+								float temperatureDecimal = sensor->Value.Value; // Pega o valor do sensor de temperatura e transforma em Float para formatar apenas com duas casas decimais.
+								Console::WriteLine(sensor->Name + ": " + temperatureDecimal.ToString("F2") + " C");
+						}
+					}
+
 			}
 		}
 
